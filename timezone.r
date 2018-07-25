aa67acbcd12a0260a41da769594a433efe919c5c
tessam30 <tessam30@users.noreply.github.com>
2018-07-25 16:26:39 -0400

    fixing Rstudio timezone issues

---
+++ timezone.r
@@ -0,0 +1,12 @@
+# to fix the warning 
+Warning message:
+In strptime(x, format, tz = tz) :
+  unknown timezone 'zone/tz/2018c.1.0/zoneinfo/America/New_York'
+  
+Set the time zone using
+Sys.setenv(TZ = "America/New_York")
+Sys.time() # to check the result
+
+
+  
+  
